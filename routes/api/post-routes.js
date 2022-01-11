af0fd54e84a2f113fe5ab23a4f55159ec72a8b81
glanctot <gregory.lanctot28@gmail.com>
2022-01-11 11:37:35 -0600

    added comment ability

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -79,14 +79,17 @@ router.post('/', (req, res) => {
 
 // put for votes
 router.put('/upvote', (req, res) => {
-// create the vote
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    })
-})
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
+  });
 
 router.put('/:id', (req, res) => {
     Post.update(
